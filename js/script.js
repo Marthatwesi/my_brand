ec87c4453f9cc40c27c5301e6fb9fbb1324716c3
Martha Twesigye <twesimartha@gmail.com>
2022-11-25 19:44:59 +0300

    added several pages

--- js/script.js
+++ js/script.js
@@ -1,41 +1,62 @@
-'use strict';
 
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+  // Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
 // navbar variables
-const nav = document.querySelector('.mobile-nav');
-const navMenuBtn = document.querySelector('.nav-menu-btn');
-const navCloseBtn = document.querySelector('.nav-close-btn');
+
+// const navMenuBtn = document.querySelector('.nav-menu-btn');
+// const navCloseBtn = document.querySelector('.nav-close-btn');
 
 
 // navToggle function
-const navToggleFunc = function () { nav.classList.toggle('active'); }
+// const navToggleFunc = function () { nav.classList.toggle('active'); }
 
-navMenuBtn.addEventListener('click', navToggleFunc);
-navCloseBtn.addEventListener('click', navToggleFunc);
+// navMenuBtn.addEventListener('click', navToggleFunc);
+// navCloseBtn.addEventListener('click', navToggleFunc);
 
 
 
 // theme toggle variables
-const themeBtn = document.querySelectorAll('.theme-btn');
+// const themeBtn = document.querySelectorAll('.theme-btn');
 
 
-for (let i = 0; i < themeBtn.length; i++) {
+// for (let i = 0; i < themeBtn.length; i++) {
 
-  themeBtn[i].addEventListener('click', function () {
+//   themeBtn[i].addEventListener('click', function () {
 
-    // toggle `light-theme` & `dark-theme` class from `body`
-    // when clicked `theme-btn`
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+//     // toggle `light-theme` & `dark-theme` class from `body`
+//     // when clicked `theme-btn`
+//     document.body.classList.toggle('light-theme');
+//     document.body.classList.toggle('dark-theme');
 
-    for (let i = 0; i < themeBtn.length; i++) {
+//     for (let i = 0; i < themeBtn.length; i++) {
 
-      // When the `theme-btn` is clicked,
-      // it toggles classes between `light` & `dark` for all `theme-btn`.
-      themeBtn[i].classList.toggle('light');
-      themeBtn[i].classList.toggle('dark');
+//       // When the `theme-btn` is clicked,
+//       // it toggles classes between `light` & `dark` for all `theme-btn`.
+//       themeBtn[i].classList.toggle('light');
+//       themeBtn[i].classList.toggle('dark');
 
-    }
+//     }
 
-  })
+//   })
 
-}
+// }
+
+
